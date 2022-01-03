95e6f3ae3ee11884dab34d6127988cc78eafaadf
agnel rayan <agnelrayan@gmail.com>
2022-01-03 16:50:32 +0530

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject_Jan2022")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject_Jan2022")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("af9e25e6-8992-4624-a20a-83f125945404")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+
+
+
+
+
+
+namespace UnitTestProject_Jan2022
+{
+    [TestClass]
+    public class UnitTest1
+    {
+       
+        [TestMethod]
+        public void TestName()
+        {
+
+            Console.WriteLine("Helo");
+
+           
+
+        }
+    }
+}
